21d1d11758c396d30986462c766605cd70b53e4d
estudiante <estudiante@ISIS2603-329.sis.virtual.uniandes.edu.co>
2018-10-18 12:44:52 -0500

    Ya se encuentra lista la preparación del front junto a algunos módulos creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { UsuarioModule } from './usuario/usuario.module';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      UsuarioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/usuario/usuario.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsuarioListComponent } from './usuario-list/usuario-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [UsuarioListComponent],
+    exports:[UsuarioListComponent]
+})
+export class UsuarioModule { }
