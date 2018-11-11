409f3b596a38b42fde130e097ce97f2f1f818990
estudiante <estudiante@ISIS2603-339.sis.virtual.uniandes.edu.co>
2018-11-10 22:00:25 -0500

    Merge conflict

--- src/app/usuario/usuario.module.ts
+++ src/app/usuario/usuario.module.ts
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { UsuarioListComponent } from './usuario-list/usuario-list.component';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppRoutingModule} from '../app-routing/app-routing.module';
-import {HttpClientModule} from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
-import {UsuarioService} from './usuario.service';
-
-@NgModule({
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    CommonModule,
-    FormsModule  ],
-  declarations: [UsuarioListComponent],
-  providers: [UsuarioService],
-  bootstrap: [UsuarioListComponent]
-})
-export class UsuarioModule { }
-=======
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsuarioListComponent } from './usuario-list/usuario-list.component';
@@ -45,4 +22,3 @@ import {UsuarioDetailComponent} from './usuario-detail/usuario-detail.component'
   bootstrap: [UsuarioListComponent]
 })
 export class UsuarioModule { }
->>>>>>> master
