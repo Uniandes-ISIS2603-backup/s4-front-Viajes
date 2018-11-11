add65adf31640e613806b34fc1beb08780c8b8b4
Nicolás <n.segura@uniandes.edu.co>
2018-11-10 20:39:39 -0500

    Agregando componentes

--- src/app/actividad/actividad-create/actividad-create.component.ts
+++ src/app/actividad/actividad-create/actividad-create.component.ts
@@ -15,59 +15,61 @@ import { Actividad } from '../actividad';
 })
 export class ActividadCreateComponent implements OnInit {
 
-    /**
-    * Constructor for the component
-    * @param editorialService The editorials' services provider
-    * @param toastrService The toastr to show messages to the user 
-    */
-    constructor(
-        private actividadService: ActividadService,
-        private toastrService: ToastrService,
-        private router: Router
-    ) { }
+  /**
+   * Constructor for the component
+   * @param editorialService The editorials' services provider
+   * @param toastrService The toastr to show messages to the user
+   */
+  constructor(
+    private actividadService: ActividadService,
+    private toastrService: ToastrService,
+    private router: Router
+  ) {
+  }
 
-    /**
-    * The new editorial
-    */
-    actividad: Actividad;
+  /**
+   * The new editorial
+   */
+  actividad: Actividad;
 
-    /**
-    * The output which tells the parent component
-    * that the user no longer wants to create an editorial
-    */
-    @Output() cancel = new EventEmitter();
+  /**
+   * The output which tells the parent component
+   * that the user no longer wants to create an editorial
+   */
+  @Output() cancel = new EventEmitter();
 
-    /**
-    * The output which tells the parent component
-    * that the user created a new editorial
-    */
-    @Output() create = new EventEmitter();
+  /**
+   * The output which tells the parent component
+   * that the user created a new editorial
+   */
+  @Output() create = new EventEmitter();
 
-    /**
-    * Creates an activity
-    */
-    createActividad(): Actividad {
-        console.log(this.actividad);
-        this.actividadService.createActividad(this.actividad)
-            .subscribe((actividad) => {
-                this.actividad = actividad;
-                this.create.emit();
-                this.toastrService.success("La actividad fue creada", "Creacion de actividad");
-                
-            });
-            return this.actividad;
-    }
-    /**
-    * Informs the parent component that the user no longer wants to create an editorial
-    */
-    cancelCreation(): void {
-        this.cancel.emit();
-    }
+  /**
+   * Creates an activity
+   */
+  createActividad(): Actividad {
+    console.log(this.actividad);
+    this.actividadService.createActividad(this.actividad)
+      .subscribe((actividad) => {
+        this.actividad = actividad;
+        this.create.emit();
+        this.toastrService.success("La actividad fue creada", "Creacion de actividad");
 
-    /**
-    * This function will initialize the component
-    */
-    ngOnInit() {
-        this.actividad = new Actividad();
-    }
+      });
+    return this.actividad;
+  }
+
+  /**
+   * Informs the parent component that the user no longer wants to create an editorial
+   */
+  cancelCreation(): void {
+    this.cancel.emit();
+  }
+
+  /**
+   * This function will initialize the component
+   */
+  ngOnInit() {
+    this.actividad = new Actividad();
+  }
 }
