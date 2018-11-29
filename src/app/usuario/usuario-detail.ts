45dbc48ad507b7523aa55c5574ed8ce41d74cbeb
estudiante <estudiante@ISIS2603-339.sis.virtual.uniandes.edu.co>
2018-11-28 22:03:20 -0500

    Cambios en UsuarioDetail

--- src/app/usuario/usuario-detail.ts
+++ src/app/usuario/usuario-detail.ts
@@ -1,9 +1,17 @@
 import {Usuario} from './usuario';
-import {CarritoCompras} from './CarritoCompras';
 import {Entrada} from './entrada';
+import {Medalla} from '../medalla/medalla';
+import {Combo} from '../combo/combo';
+import {Pago} from '../pago/pago';
 
 export class UsuarioDetail extends Usuario {
 
     entradas: Entrada[];
+    
+    medallas: Medalla[];
+    
+    combos: Combo[];
+    
+    pagos: Pago[];
 
 }
